e1d30d305a8702fa81ea780ecde8b8f284ad36b0
LincolnCamarini <lcamarini@hotmail.com>
2021-02-04 16:17:21 -0300

    w

--- index.php
+++ index.php
@@ -1,14 +1,27 @@
-<h1>Bem vindo turma 14A do itaú pelo treinamento da Gama </h1>
+<h1>Bem vindo turma 14A do itaú pele treinamento da Gama </h1>
 
-<h1> Um deploy automático feito com alunos do itaú </h1>
+<h3> Um deploy aotomático feito com alunos do itaú </h3>
 
 <div style="display:flex">
-<div style="width:200px;height:200px;background:yellow; padding:50px;margin:50px">
-    Um teste em um quadrado amarelo
-    </div>
- <div style="width:200px;height:200px;background:green;color:white;padding:10px;margin:10px">  
+  <div style="width:200px;height:400px;background:yellow;padding:50px;margin:10px">
+    Um teste em um quadrado
     <br>
-    <img arc="c1.jpg" style="width: 200px">
-    </div>
-    </div>
-   
+    <img src="c1.jpg" style="width: 200px">
+    <br><br>
+    R$ 550,00
+    <br><br>
+    <a href="https://pag.ae/7WRynf6X1" target="_blank" style="display:block;background:green;color:white;text-align:center;padding:20px;border-radius:10px">Comparar agora</a><br>
+	<a href="https://api.whatsapp.com/send?phone=55999999999999&text=ol%C3%A1%20vendedor%20quero%20comprar%20de%20vc" target="_blank" >Entrar em contato com o vendedor</a>
+  </div>
+
+  <div style="width:200px;height:400px;background:blue;color:white;padding:50px;margin:10px">
+    Um teste em um quadrado
+    <br>
+    <img src="c2.jpg" style="width: 200px">
+   <br><br>
+    R$ 850,00
+    <br><br>
+    <a href="https://pag.ae/7WRynf6X1" target="_blank" style="display:block;background:green;color:white;text-align:center;padding:20px;border-radius:10px">Comparar agora</a><br>
+	<a href="https://api.whatsapp.com/send?phone=55999999999999&text=ol%C3%A1%20vendedor%20quero%20comprar%20de%20vc" target="_blank" style="color:white">Entrar em contato com o vendedor</a>
+  </div>
+</div>
