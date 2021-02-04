a076f3a99b72858a51fe06ff54138dbf2cfa71bf
LincolnCamarini <lcamarini@hotmail.com>
2021-02-04 15:21:03 -0300

    x

--- index.php
+++ index.php
@@ -3,14 +3,10 @@
 <h1> Um deploy automático feito com alunos do itaú </h1>
 
 <div style="display:flex">
-<div style="width:100px;height:100px;background:yellow; padding:50px;margin:50px">
+<div style="width:200px;height:200px;background:yellow; padding:50px;margin:50px">
     Um teste em um quadrado amarelo
     </div>
- <div style="width:100px;height:100px;background:green;color:white;padding:10px;margin:10px">
-    Um teste em um quadrado verde
-   </div>
-    <div style="width:100px;height:100px;background:violet">
-    Um teste em um quadrado violeta
+ <div style="width:200px;height:200px;background:green;color:white;padding:10px;margin:10px">  
     <br>
     <img arc="c1.jpg" style="width: 200px">
     </div>
